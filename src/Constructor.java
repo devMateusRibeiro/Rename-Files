ba27b65e67912e4ae88dd175d396af2556456313
devMateusRibeiro <contatomateusribeiromineiro@gmail.com>
2022-06-22 21:10:48 -0300

    Inputs no construtor.

--- src/Constructor.java
+++ src/Constructor.java
@@ -9,7 +9,11 @@ public class Constructor {
 
         File sourceFolder = new File(JOptionPane.showInputDialog("Pasta de origem: "));
         File destinationFolder = new File(JOptionPane.showInputDialog("Pasta de destino: "));
+        String tvShowName = JOptionPane.showInputDialog("Nome: ");
+        String releaseYear = JOptionPane.showInputDialog("Ano: ");
+        String season = JOptionPane.showInputDialog("Temporada: ");
+        String extension = JOptionPane.showInputDialog("Tipo: ");
 
-        renameFiles.Rename(sourceFolder,destinationFolder, teste,teste,teste,teste);
+        renameFiles.Rename(sourceFolder, destinationFolder, tvShowName, releaseYear, season, extension);
     }
 }
--- src/RenameFiles.java
+++ src/RenameFiles.java
@@ -1,12 +1,12 @@
 import java.io.File;
 public class RenameFiles {
-    public void Rename(File sourceFolder,File destinationFolder, String tvShowName, String releaseYear, String sXeX, String extension){
+    public void Rename(File sourceFolder,File destinationFolder, String tvShowName, String releaseYear, String season, String extension){
         if (sourceFolder.isDirectory() && destinationFolder.isDirectory()){
             System.out.println("Renomeando arquivos! Aguarde...");
 
             File[] fileList = sourceFolder.listFiles();
             for(int i = 0; i < fileList.length; i++){
-                fileList[i].renameTo(new File(destinationFolder.getPath() + "\\" +  releaseYear + ".mp4"));
+                fileList[i].renameTo(new File(destinationFolder.getPath() + "\\" +  tvShowName + " (" + releaseYear + ") - s" + season + "e" + (i+1) + extension));
             }
         }else{
             System.out.println("Folder not found!");
